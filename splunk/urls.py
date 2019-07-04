b84a089e044ee675f0c29562d0125ee49e68aa97
lzy10347 <2399262753@qq.com>
2019-07-04 23:16:57 +0800

    change urls.py views.py

--- splunk/urls.py
+++ splunk/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from teacher import views as tv
 urlpatterns = [
     # Examples:
     # url(r'^$', 'splunk.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin1/', do_admin1),
-    url(r'^admin2/', do_admin2),
+    url(r'^normalmap/', tv.do_normalmap),
+    url(r'^withparam/(?P<year>[0-9]{4})/(?P<month>[0,1][0,9])', tv.withparam),
 ]
